e23d415947a1224d727151c994bdca7b9cc70740
Mitch Alforque <malforque717@gmail.com>
2018-10-11 00:01:04 +0800

    new sql

--- project.sql
+++ project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 10, 2018 at 04:27 PM
+-- Generation Time: Oct 10, 2018 at 06:00 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.10
 
@@ -44,7 +44,22 @@ CREATE TABLE `students` (
 INSERT INTO `students` (`id`, `studentId`, `firstName`, `lastName`, `contactNo`, `address`) VALUES
 (3, 18100001, 'Ruvejen', 'Enot', '14288484', 'Pardo'),
 (4, 18100001, 'Mitch', 'Alforque', '09567130760', 'Tabada'),
-(5, 18100001, 'Merlrose', 'Evin', '09567130760', 'Barangay Guba Pit\'os');
+(5, 18100001, 'Merlrose', 'Evin', '09567130760', 'Barangay Guba Pit\'os'),
+(6, 18100001, 'Karl', 'Kazuksi', '123455', 'Sa Ila'),
+(7, 18100002, 'No', 'Name', '14288484', 'Just me'),
+(8, 18100003, 'Test', 'test', '1420000', 'Pardo');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teachers`
+--
+
+CREATE TABLE `teachers` (
+  `id` int(11) NOT NULL,
+  `firstName` int(11) NOT NULL,
+  `lastName` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -68,7 +83,10 @@ INSERT INTO `users` (`id`, `avatar`, `username`, `password`, `status`) VALUES
 (3, 'gwapa1.jpg', '18100001', '18100001', 1),
 (4, 'gwapa2.jpg', '18100001', '18100001', 1),
 (5, 'gwapa3.jpg', '18100001', '18100001', 1),
-(6, '', 'admin', 'admin', 1);
+(6, '', 'admin', '$2y$10$0ghBJcF9kcLcC3SkRpY3F.88RJgqjK.0HpgEaLGWOVCUJ4Q7uPwCm', 1),
+(7, 'gwapa4.jpg', '18100001', '18100001', 1),
+(8, 'gwapa12.jpg', '18100002', '18100002', 1),
+(9, 'gwapa14.jpg', '18100003', '$2y$10$P/gKBW4nr9QAfOUDmmu3Hum/3KtzVAnKsAHwa5OYVpFLcNCFof2mW', 1);
 
 -- --------------------------------------------------------
 
@@ -89,7 +107,22 @@ CREATE TABLE `user_student` (
 INSERT INTO `user_student` (`id`, `studentId`, `userId`) VALUES
 (3, 3, 3),
 (4, 4, 4),
-(5, 5, 5);
+(5, 5, 5),
+(6, 6, 7),
+(7, 7, 8),
+(8, 8, 9);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_teacher`
+--
+
+CREATE TABLE `user_teacher` (
+  `id` int(11) NOT NULL,
+  `teacherId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -99,6 +132,12 @@ INSERT INTO `user_student` (`id`, `studentId`, `userId`) VALUES
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `teachers`
+--
+ALTER TABLE `teachers`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -114,6 +153,12 @@ ALTER TABLE `user_student`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user_teacher`
+--
+ALTER TABLE `user_teacher`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -121,19 +166,31 @@ ALTER TABLE `user_student`
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `teachers`
+--
+ALTER TABLE `teachers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `user_student`
 --
 ALTER TABLE `user_student`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `user_teacher`
+--
+ALTER TABLE `user_teacher`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
